48e8c4cadabc7b1cc55464dec7205533b6463f90
redouane <redouane.hitachi@gmail.com>
2024-04-04 19:20:14 -0700

    09 - Creating and Saving User Data

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -8,4 +15,16 @@ export class User {
   email: string;
   @Column()
   password: string;
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id', this.id);
+  }
 }
